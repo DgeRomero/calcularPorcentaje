4fd1f0ba632f0793a57dfea5b30a89cbdfabde4b
Diego <d.-romero@outlook.com>
2024-04-04 21:01:12 -0300

    poner que porcentaje desea calcular

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let precio = document.getElementById("texto");
 let btnCalcular = document.getElementById("btnCalcular");
 let resultado = document.getElementById("resultado");
+let porcentaje = document.getElementById("porcentaje");
 
 function calcularPorcentaje(){
     let monto = precio.value;
-    resultado.innerHTML = "Lo que tengo que cobrar es: $" + monto*1.4;
+    let adicional = porcentaje.value;
+    let subtotal = monto*adicional/100;
+    let total = Number(monto)+ Number(subtotal);
+    resultado.innerHTML = "Lo que tengo que cobrar es: $" + total;
 }
